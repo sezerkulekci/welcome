392be62442d267d3d812dd4e6c16633fbd5a1c14
Sezer Kulekci <sezerklkci@gmail.com>
2023-12-19 15:01:10 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hosgeldin, { Hosgeldin1 } from "./test";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hosgeldin isim="Sezer1" />
+      <Hosgeldin1 isim="Semih" />
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Hosgeldin1 = ({ isim }) => {
+  console.log(isim);
+  return <h1>{isim} Hoşgeldin1</h1>;
+};
+
+const Hosgeldin = ({ isim }) => {
+  console.log(isim);
+  return <h1>{isim} Hoşgeldin</h1>;
+};
+export default Hosgeldin;
